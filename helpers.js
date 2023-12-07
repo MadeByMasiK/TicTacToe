28a0f27363bcfecfb2ba852fbca52bc1fe5bfe7e
MadeByMasiK <59820807+MadeByMasiK@users.noreply.github.com>
2023-12-07 18:28:16 +0200

    kommentoitu scipt.js, player.js ja helpers.js sekä poistettu toivon mukaan turhaa sisältöä

--- helpers.js
+++ helpers.js
@@ -1,24 +1,23 @@
 //Helpers (from http://jaketrent.com/post/addremove-classes-raw-javascript/)
+
+// tämä funktio tarkastaa onko annetulla HTML elementillä (parametri el) class (parametri className) ja palauttaa tiedon boolean-muodossa
 export function hasClass(el, className) {
     if (el.classList) return el.classList.contains(className);
+    // vaihtoehtoinen koodi, jos käytetty selain ei tue classList ominaisuutta
     else return !!el.className.match(new RegExp("(\\s|^)" + className + "(\\s|$)"));
 }
 
+// tämä funktio lisää annetulle HTML elementille (parametri el) classin (parametri className)
 export function addClass(el, className) {
     if (el.classList) el.classList.add(className);
+    // vaihtoehtoinen koodi, jos käytetty selain ei tue classList ominaisuutta
     else if (!hasClass(el, className)) el.className += " " + className;
-}
-
-export function removeClass(el, className) {
-    if (el.classList) el.classList.remove(className);
-    else if (hasClass(el, className)) {
-        var reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
-        el.className = el.className.replace(reg, " ");
-    }
 }
 
 //Helper function that takes the object returned from isTerminal() and adds a
 //class to the board that will handle drawing the winning line's animation
+
+// helper-funktio, joka ottaa funktion isTerminal() palauttaman objektin ja lisää pelilaudalle classin joka piirtää voittavan linjan animaation
 export function drawWinningLine(statusObject) {
     if (!statusObject) return;
     const { winner, direction, row, column, diagonal } = statusObject;
